b5b5b1816150a3c6dc33e739dd49596d363b3e5a
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-10-15 04:55:08 +0300

    new commit

--- Booking/src/app/views/hotels/booking/components/offer-discount/offer-discount.component.ts
+++ Booking/src/app/views/hotels/booking/components/offer-discount/offer-discount.component.ts
@@ -1,12 +1,21 @@
+import { StickyDirective } from '@/app/directives/sticky.directive'
 import { currency } from '@/app/store'
 import { Component } from '@angular/core'
+import { ScrollToModule, ScrollToService } from '@nicky-lenaers/ngx-scroll-to'
 
 @Component({
   selector: 'booking-offer-discount',
   standalone: true,
-  imports: [],
+  imports: [StickyDirective,ScrollToModule],
   templateUrl: './offer-discount.component.html',
-  styles: ``,
+  styles: `
+  @media (min-width: 992px) {
+    .sticky-card {
+      position: sticky;
+      top: 20px;
+    }
+  }`,
+  providers: [ScrollToService],
 })
 export class OfferDiscountComponent {
   currencyType = currency
