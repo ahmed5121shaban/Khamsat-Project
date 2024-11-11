9cd2c892f83a0ea0b5c3945ca7f7808e76f340cd
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-11-11 10:26:12 +0200

    new commit

--- Booking/src/app/views/hotels/list/components/listcard/listcard.component.ts
+++ Booking/src/app/views/hotels/list/components/listcard/listcard.component.ts
@@ -24,63 +24,66 @@ import { CommonModule } from '@angular/common'
   ],
   templateUrl: './listcard.component.html',
   styles: `
-@media (max-width: 780px) {
-  .responsive-text{
-      flex-direction: column;
+    @media (max-width: 780px) {
+      .responsive-text {
+        flex-direction: column;
+      }
+      .responsive-text i {
+        margin: 0 !important;
+      }
+      .adress {
+        width: 100%;
+      }
     }
-    .responsive-text i{
-      margin: 0 !important;
-    }
-    .adress{
-      width:100%;
-    }
-  }
 
-  @media (min-width: 576px) {
-    .responsive-text {
-      width: 80%;
-      font-size: 0.9rem;
+    @media (min-width: 576px) {
+      .responsive-text {
+        width: 80%;
+        font-size: 0.9rem;
+      }
     }
-  }
 
-  @media (min-width: 768px) {
-    .responsive-text {
-      width: 100%;
-      font-size: 14px;
+    @media (min-width: 768px) {
+      .responsive-text {
+        width: 100%;
+        font-size: 14px;
+      }
+      .view-map {
+        font-size: 13px;
+      }
     }
-    .view-map{
-      font-size:13px;
-    }
-  }
     .loader {
-  width: 400px;
-  height: 16px;
-  border-radius: 20px;
-  border: 2px solid;
-  position: relative;
-}
-.loader::before {
-  content: "";
-  position: absolute;
-  margin: 2px;
-  inset: 0 100% 0 0;
-  border-radius: inherit;
-  background: currentColor;
-  animation: l6 1s;
-}
-@keyframes l6 {
-    100% {inset:0}
-}`,
+      width: 400px;
+      height: 16px;
+      border-radius: 20px;
+      border: 2px solid;
+      position: relative;
+    }
+    .loader::before {
+      content: '';
+      position: absolute;
+      margin: 2px;
+      inset: 0 100% 0 0;
+      border-radius: inherit;
+      background: currentColor;
+      animation: l6 1s;
+    }
+    @keyframes l6 {
+      100% {
+        inset: 0;
+      }
+    }
+  `,
 })
 export class ListcardComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
-      this.isLoading = false;
-    }, 1000);
+      this.isLoading = false
+    }, 1000)
   }
   hotelList = hotels
   currencyType = currency
-  isLoading: boolean = true;
+  isLoading: boolean = true
   @ViewChild('listSlider', { static: false }) listSlider: any
 
   listSliderSettings: TinySliderSettings = {
